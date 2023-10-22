a5fcd07ae8c44fa20594fd9f7d0a8a098293d1f1
Pratik <agrawalpa_4@rknec.edu>
2023-10-22 18:57:51 +0530

    Hello World Program

---
+++ helloWorld.cpp
@@ -0,0 +1,9 @@
+#include <iostream> // iostream is a header library which contains pre-build functions
+using namespace std; // defining std namespace for cout
+
+// main fuctions starts
+int main(){ // int is the return type 
+    cout<<"Hello World!";
+    return 0; // since our return type was int hence returning 0
+}
+// main fuctions ends
